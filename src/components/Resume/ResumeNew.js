207b01cbadc52b582a4699a9a960a1db67e118bb
alfieatkinson <alf.atkinson13@gmail.com>
2025-02-03 15:23:31 +0000

    Update ResumeNew component to dynamically set PDF URL

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -10,8 +9,13 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
+  const [pdfUrl, setPdfUrl] = useState("");
 
+  // Set the PDF URL dynamically
   useEffect(() => {
+    const githubPdfUrl = "https://raw.githubusercontent.com/alfieatkinson/Curriculum-Vitae/main/main.pdf";
+    setPdfUrl(githubPdfUrl);
+
     setWidth(window.innerWidth);
   }, []);
 
@@ -22,7 +26,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={pdfUrl}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -32,7 +36,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
+          <Document file={pdfUrl} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
@@ -40,7 +44,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={pdfUrl}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
