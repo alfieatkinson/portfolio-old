0910432868aa37ad05692771d6100d35d0c17558
alfieatkinson <alf.atkinson13@gmail.com>
2025-02-06 16:06:34 +0000

    Implement EducationCard component with title display

--- src/components/About/EducationCard.js
+++ src/components/About/EducationCard.js
@@ -1,3 +1,14 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+
+function EducationCard(props) {
+  return (
+    <Card className="education-card-view">
+      <Card.Title style={{textAlign: "left"}}>{props.title}</Card.Title>
+
+    </Card>
+  )
+}
+
+export default EducationCard;
