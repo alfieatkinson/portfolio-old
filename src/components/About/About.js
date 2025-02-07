69498147bb41df135e0a75bcf997ae12d266407a
alfieatkinson <alf.atkinson13@gmail.com>
2025-02-07 15:38:41 +0000

    Add detailed EducationCard components for Master's and Bachelor's degrees with relevant information

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -46,16 +46,68 @@ function About() {
         </h1>
 
         <Toolstack />
-
         <Github />
+
+        <br />
 
         <h1 className="project-heading">
           Where I've <strong className="purple">Learned</strong>
         </h1>
 
-        <Educationcard
-          title="Master of Science in Computer Science"
-        />
+        <br />
+
+        <Row>
+          <Col>
+            <Educationcard
+              title="Master of Science in Computer Science"
+              issuer="University of Lincoln"
+              dateRange="2024 - 2025"
+              grade="Predicted: Distinction (AVG: 84%)"
+              activities={[
+                "Computer Science Society Member",
+                "Hackathon Participant"
+              ]}
+              modules={[
+                "Advanced Artificial Intelligence",
+                "Advanced Machine Learning",
+                "Computer Vision",
+                "Information Systems Security",
+                "Programming Principles",
+                "Research Methods",
+                "Research Project",
+                "Software Engineering"
+              ]}
+            />
+          </Col>
+          <Col>
+            <Educationcard
+              title="Bachelor of Science in Computer Science"
+              issuer="University of Lincoln"
+              dateRange="2021 - 2024"
+              grade="First Class Honours"
+              activities={[
+                "Computer Science Society Member",
+                "Hackathon Participant",
+                "UKIEPC Participant"
+              ]}
+              modules={[
+                "Advanced Programming",
+                "Algorithms and Complexity",
+                "Artificial Intelligence",
+                "Cloud Computing",
+                "Logic and Computation",
+                "Machine Learning",
+                "Network Fundamentals",
+                "Parallel Programming",
+                "Scalable Database Systems",
+                "Team Software Engineering",
+                "User Experience Design"
+              ]}
+            />
+          </Col>
+        </Row>
+
+        <br />
       </Container>
     </Container>
   );
