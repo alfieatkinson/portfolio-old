81bf0a410196c54f412625642dc6fc9c4c72ec87
alfieatkinson <alf.atkinson13@gmail.com>
2025-02-07 15:38:34 +0000

    Enhance EducationCard component with additional details and layout improvements

--- src/components/About/EducationCard.js
+++ src/components/About/EducationCard.js
@@ -1,12 +1,64 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function EducationCard(props) {
   return (
     <Card className="education-card-view">
-      <Card.Title style={{textAlign: "left"}}>{props.title}</Card.Title>
+      <Card.Body>
+        <Row>
+          <Col xs={8}>
+            <Card.Title className="purple" style={{textAlign: "left"}}>{props.title}</Card.Title>
+          </Col>
 
+          <Col>
+          <Card.Text style={{textAlign: "right"}}>
+            {props.dateRange}
+          </Card.Text>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Card.Text style={{textAlign: "left"}}>
+              {props.issuer}
+            </Card.Text>
+          </Col>
+
+          <Col>
+            <Card.Text style={{textAlign: "right"}}>
+              {props.grade}
+            </Card.Text>
+          </Col>
+        </Row>
+
+        {props.activities && props.activities.length > 0 && (
+          <div>
+            <br />
+            <Card.Subtitle style={{textAlign: "left"}}>
+              <strong className="purple">Activities:</strong>
+            </Card.Subtitle>
+
+            <Card.Text style={{textAlign: "justify"}}>
+              {props.activities.join(" • ")}
+            </Card.Text>
+          </div>
+        )}
+
+        {props.modules && props.modules.length > 0 && (
+          <div>
+            <br />
+            <Card.Subtitle style={{textAlign: "left"}}>
+              <strong className="purple">Notable Modules:</strong>
+            </Card.Subtitle>
+
+            <Card.Text style={{textAlign: "justify"}}>
+              {props.modules.join(" • ")}
+            </Card.Text>
+          </div>
+        )}
+      </Card.Body>
     </Card>
   )
 }
